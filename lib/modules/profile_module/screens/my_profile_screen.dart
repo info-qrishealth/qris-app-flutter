29e36a909bce2c795f4fb57f1ecab95bfb8fd3ef
keshav <keshav@onelap.in>
2025-01-22 23:19:04 +0530

    Dynamic data in my profile.

--- lib/modules/profile_module/screens/my_profile_screen.dart
+++ lib/modules/profile_module/screens/my_profile_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 import 'package:qris_health/constants/app_constants.dart';
 import 'package:qris_health/constants/enums/gender.dart';
+import 'package:qris_health/modules/users_module/cubits/user_cubit.dart';
 import 'package:qris_health/shared/components/common_app_bar.dart';
 import 'package:qris_health/shared/components/common_textfield.dart';
 import 'package:qris_health/shared/components/gender_input_container_row.dart';
@@ -18,10 +20,21 @@ class MyProfileScreen extends StatefulWidget {
 class _MyProfileScreenState extends State<MyProfileScreen> {
   final _textTheme = Get.textTheme;
   final _nameController = TextEditingController();
-  final _mobileController = TextEditingController();
+  final _phoneNumberController = TextEditingController();
   final _emailController = TextEditingController();
   final _weightController = TextEditingController();
   final _heightController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    final userCubit = BlocProvider.of<UserCubit>(context);
+    final user = userCubit.state.user;
+
+    _nameController.text = user.name ?? '';
+    _phoneNumberController.text = user.phone ?? '';
+    _emailController.text = user.email ?? '';
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,6 +52,7 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
                             fontWeight: FontWeight.w500))))),
         appBar: CommonAppBar(title: 'My Profile'),
         body: ListView(
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
             physics: BouncingScrollPhysics(),
             padding:
                 EdgeInsets.symmetric(horizontal: AppConstants.scaffoldPadding),
@@ -51,8 +65,9 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
                   headingText: 'Name'),
               SizedBox(height: 16),
               CommonTextField(
+                  readOnly: true,
                   hintText: 'Enter your phone number',
-                  controller: _mobileController,
+                  controller: _phoneNumberController,
                   textInputType: TextInputType.phone,
                   headingText: 'Mobile Number'),
               SizedBox(height: 16),
@@ -60,9 +75,11 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
                   hintText: 'Enter your email address',
                   controller: _emailController,
                   textInputType: TextInputType.emailAddress,
+                  readOnly: true,
                   headingText: 'Email'),
               SizedBox(height: 16),
-              GenderInputContainerRow(onTap: (gender) {}, selectedGender: Gender.MALE),
+              GenderInputContainerRow(
+                  onTap: (gender) {}, selectedGender: Gender.MALE),
               SizedBox(height: 16),
               CommonTextField(
                   hintText: 'Enter your body weight',
