8e2bc629d845a68d67a367815b07e920935987fa
KeshuRathore99 <keshurathore99@gmail.com>
2025-07-18 19:12:05 +0530

    Push notification integrated.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 import 'package:qris_health/modules/all_scans_module/cubits/tests_category_cubit.dart';
 import 'package:qris_health/modules/health_article_module/cubits/health_article_category_cubit/health_article_category_cubit.dart';
@@ -19,6 +23,14 @@ import 'package:qris_health/styles/app_styles.dart';
 import 'firebase_options.dart';
 import 'modules/refer_and_earn_module/cubits/qris_wallet_cubit/qris_wallet_cubit.dart';
 
+FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+@pragma('vm:entry-point')
+Future<void> _myBackgroundMessageHandler(RemoteMessage message) async {
+  await _showNotification(message);
+}
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -29,8 +41,19 @@ Future<void> main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _firebaseCloudMessagingListeners();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,5 +76,55 @@ class MyApp extends StatelessWidget {
             title: 'Qris Health',
             theme: AppStyles.theme(context),
             home: CustomSplashScreen()));
+  }
+
+  Future<void> _firebaseCloudMessagingListeners() async {
+    try {
+      if (Platform.isIOS) {
+        FirebaseMessaging.instance.requestPermission(
+            sound: true, badge: true, alert: true, provisional: false);
+      }
+
+      if (Platform.isAndroid) {
+        FirebaseMessaging.onBackgroundMessage(_myBackgroundMessageHandler);
+      }
+    } catch (e) {}
+
+    FirebaseMessaging.onMessage.listen((event) async {
+      try {
+        await _showNotification(event);
+      } catch (e) {
+        print(e.toString());
+      }
+    });
+  }
+}
+
+Future<void> _showNotification(RemoteMessage message) async {
+  try {
+    final title =
+        GetPlatform.isIOS ? message.notification?.title : message.data['title'];
+    final body =
+        GetPlatform.isIOS ? message.notification?.body : message.data['body'];
+
+    AndroidNotificationDetails androidNotificationDetailsWithDefaultSound =
+        const AndroidNotificationDetails('1', 'channel name',
+            channelDescription: 'channel description',
+            icon: '@mipmap/ic_launcher',
+            playSound: true,
+            priority: Priority.high,
+            importance: Importance.high);
+
+    NotificationDetails platformChannelSpecifics = NotificationDetails(
+        android: androidNotificationDetailsWithDefaultSound,
+        iOS: const DarwinNotificationDetails());
+
+    _flutterLocalNotificationsPlugin.show(
+        DateTime.now().millisecondsSinceEpoch.remainder(100000),
+        title,
+        body,
+        platformChannelSpecifics);
+  } catch (e) {
+    print(e);
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import firebase_core
 import firebase_messaging
+import flutter_local_notifications
 import flutter_udid
 import path_provider_foundation
 import share_plus
@@ -17,6 +18,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterUdidPlugin.register(with: registry.registrar(forPlugin: "FlutterUdidPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
